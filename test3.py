ce9a7b78e0ebb605c28e4000ab2ec6f8ccb96784
greedyboy <greedyboy@163.com>
2019-04-19 00:54:27 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- test3.py
+++ test3.py
@@ -1,2 +1,32 @@
-import os
-os.system('git help --a')
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+"""info
+
+"""
+
+import os,time,random
+from time import sleep
+
+def gitpush(fn,commit='by q',delfn=True):
+    os.system('git add '+fn)
+    os.system('git commit -m "'+ commit +'"')
+    os.system('git push')
+    if delfn:
+        os.remove(fn)
+
+
+def creat_file(strx=''):
+    randx=random.randint(1000, 9999)
+    fn= 'blog/'+ time.strftime("%Y%m%d%H%M%S", time.localtime())+str(randx)+'.txt'
+    with open(fn, 'a+') as f:
+        f.write(fn + '\n')  # 加\n换行显示
+        f.write(strx)
+    return fn
+strlist=['how','are','you']
+
+for strx in strlist:
+    fn=creat_file(strx=strx)
+    print('pushing ' + fn)
+    gitpush(fn=fn,commit=strx)
+    sleep(2)
+
